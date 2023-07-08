e0da60a914eb7402076fc752604711dfc24cdc9e
AlPichDev <lexus190594@gmail.com>
2023-07-08 17:06:32 +0300

    08.07.2023 create armor

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const swiper = new Swiper('.swiper', {
+
+
+const swiper1 = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -7,16 +9,16 @@ const swiper = new Swiper('.swiper', {
         maxRatio: 3,
     },
 
-    effect: 'cards',
-    cardsEffect: {
-        slideShadows: false,
-        perSlideOffset: 1,
-        perSlideRotate: 3,
-    },
+    // effect: 'cards',
+    // cardsEffect: {
+    //     slideShadows: false,
+    //     perSlideOffset: 1,
+    //     perSlideRotate: 3,
+    // },
     // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
 
     // Navigation arrows
     navigation: {
@@ -25,5 +27,35 @@ const swiper = new Swiper('.swiper', {
     },
 
     // And if we need scrollbar
-    scrollbar: false,
+    
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    zoom: {
+        maxRatio: 3,
+    },
+
+    // effect: 'cards',
+    // cardsEffect: {
+    //     slideShadows: false,
+    //     perSlideOffset: 1,
+    //     perSlideRotate: 3,
+    // },
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    
 });
